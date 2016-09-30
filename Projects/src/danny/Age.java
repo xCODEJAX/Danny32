3e805ffc3fd789f6f15ab09feef919241bf57243
Danny <xxsilentninjazxx@hotmail.com>
2016-09-30 13:07:41 -0400

    ArithmaticExpresstions 1-5

--- Projects/src/danny/Age.java
+++ Projects/src/danny/Age.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 /**
  * ICS3C
  * 27/09/2016
- *ArithmaticExpressions
- *Danny Lang
+ * ArithmaticExpressions
+ * Danny Lang
  */
 public class Age {
 
@@ -21,23 +21,28 @@ public class Age {
 		/**
 		 *  Scanner
 		 */
+		@SuppressWarnings("resource")
 		Scanner scan=new Scanner(System.in);
 		int number1;
 		int number2;
 		int number3;
 		int number4;
 		int number5;
+		
 		System.out.println("Please Enter 5 numbers:");
 		number1= scan.nextInt();
 		number2= scan.nextInt();
 		number3= scan.nextInt();
 		number4= scan.nextInt();
 		number5= scan.nextInt();
+		
 		System.out.println("Your total is: " +(number1 + number2 + number3 + number4 + number5));
 		System.out.println("Second number - Third number is: "+(number2 - number3));
-	    System.out.println("Your first number * Your 5 number is: "+(number1*number5));
-	    System.out.println("Your fourth number / by your second number is:" +(number4/number2));
-	    System.out.println("Your dividing number % by the second is: " +(number4%number2));
+		System.out.println("Your first number * Your 5 number is: "+(number1*number5));
+		System.out.println("Your fourth number / by your second number is :" +(number4/number2));
+		System.out.println("Your dividing number % by the second is: " +(number4%number2));
+		System.out.println("Math.pow third number is : " +(Math.pow(number1,number3)));
+		System.out.println("The square root of the fifth number is:  " +(Math.sqrt(number5)));
 	}
-		
+
 }
