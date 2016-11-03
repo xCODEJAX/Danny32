c15d7beb65f49481b556020742bf7f0799f75fb0
Danny <xxsilentninjazxx@hotmail.com>
2016-11-03 13:02:17 -0400

    Unit 2

---
+++ Projects/src/danny/Divisible.java
@@ -0,0 +1,42 @@
+package danny;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ *Danny Lang
+ *Divisible.java
+ *October/31/2016
+ *
+ */
+public class Divisible {
+
+	/**
+	 * 
+	 */
+	public Divisible() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Scanner scan=new Scanner(System.in);
+		
+		int integers;
+		
+		System.out.println("Enter integers:");
+		integers=scan.nextInt();
+		
+		
+		if (integers % 7 != 0)
+			System.out.print("This integer is divisible by 7");
+			else
+				System.out.print("This integer is not divisible by 7");
+	}
+
+}
