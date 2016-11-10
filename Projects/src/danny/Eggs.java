9dd45c3803dc2e01ed9142fefe760581a9a47cbb
Danny <xxsilentninjazxx@hotmail.com>
2016-11-10 13:36:35 -0500

    Unit 2

--- Projects/src/danny/Eggs.java
+++ Projects/src/danny/Eggs.java
@@ -6,17 +6,29 @@ import java.util.Scanner;
 public class Eggs {
 
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		DecimalFormat df= new DecimalFormat("0.00");
+	Scanner scan=new Scanner(System.in);
+	DecimalFormat df= new DecimalFormat("0.00");
 		int eggs;
-		
-		System.out.println("Enter the amount of eggs:");
+		int dozen=12;
+		int dozensOfEggs;
+		double price=0;
+		int extra;
+	
+	System.out.println("Enter the number of eggs purchased : "); 
 		eggs=scan.nextInt();
-		
-		if (eggs<=99){
-			System.out.println("Enter the number of eggs purchased: 0.50");
-			System.out.println("The bill is equal to:$" + df.format (0.50*eggs));
-		}
+		dozensOfEggs= eggs/dozen;
+	
+		if (dozensOfEggs >11)
+		price=0.35; 
+		else if (dozensOfEggs <11)
+		price=0.40;
+		else if (dozensOfEggs <6)
+		price=0.45; 
+		else if (dozensOfEggs <4)
+		price=0.50;
+		extra=eggs%12;
+	
+	System.out.println("The bill is equal to:" +df.format (price*dozensOfEggs+price/12*extra));
 		
 		
 
